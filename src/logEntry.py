136655e006fb311bbe6089f8ae7d7e33fc6de317
threep <alangton1337@gmail.com>
2025-08-16 11:13:47 -0400

    added all of the regex logic to extract postfix info

--- src/logEntry.py
+++ src/logEntry.py
@@ -1,7 +1,7 @@
 
 class logEntry:
 
-    def __init__(self):
+    def __init__(self,messageID,mailServer,clientName,clientIP,subject,sender,recipient,status,protocol):
         pass
 
     def __repr__(self):
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@
 # https://www.geeksforgeeks.org/python/command-line-arguments-in-python/
 import sys
 import argparse
+import re
 
 # First goal is to just extract text from the log file and print out log lines
 # Worry about making it more complex afterwards
@@ -11,7 +12,8 @@ import argparse
 # Later on I can deal with ingesting the logs into a database.
 
 
-# First basic goal should be to parse a file containing only one message, and create a list of objects that represent entries
+# First basic goal should be to parse a file containing only one message, 
+# and create a list of objects that represent entries
 
 
 # Here is some AI generated program outline:
@@ -59,10 +61,74 @@ def main():
         lines = f.readlines()
     #print(f"{file_contents}")
 
-    x=0
+    #Extract the message ID in the line
+    #Message IDs are 11 characters and look to contain 0-9 and A-F
+    #[0-9,A-F]{11}
+
+    #Client name
+    #Client IP
+    #Extract from the capture groups
+    #(?<=client=)([^[]+)\[([^]]+)
+
+    #subject
+    #(?<=Subject: ).+(?= from.+\[.+\])
+
+    #from
+    # (?<=[0-9,A-F]{11}: from=<)[^>]+
+    #to
+    #(?<=[0-9,A-F]{11}: to=<)[^>]+
+
+    #status
+    # (?<=status=)[^ ]+
+
+    #Mail server name
+
+    #^[^ ]+\s([^ ]+)
+    
+    # If a message ID is found, all subsequent lines will contain the message ID
+    # the next line that doesn't is a different entry
+    currentMessageId = None
     for line in lines:
-        x+=1
-        print(f"line {x}:{line}")
+        #This is bad, I can't assume group 0 will exist, i first need to do the regex, then test if it found anything
+        messageID = re.search("[0-9,A-F]{11}",line).group(0)
+        if messageID == currentMessageId:
+            print(f"Found current message id {messageID}")
+            
+            subject = re.search("(?<=Subject: ).+(?= from.+\[.+\])",line)
+            if (subject):
+                print (f"Found subject: {subject.group(0)}")
+
+            sender = re.search("(?<=[0-9,A-F]{11}: from=<)[^>]+",line)
+            if (sender):
+                print (f"Found sender: {sender.group(0)}")
+            
+            
+            recipient = re.search("(?<=[0-9,A-F]{11}: to=<)[^>]+",line)
+            if (recipient):
+                print (f"Found recipient: {recipient.group(0)}")
+
+            status = re.search("(?<=status=)[^ ]+",line)
+            if (status):
+                print (f"Found status: {status.group(0)}")
+
+            protocol = re.search("(?<=proto=)[^ ]+",line)
+            if (protocol):
+                print (f"Found protocol: {protocol.group(0)}")
+
+        elif messageID:
+            print(f"Found new message id {messageID}")
+            currentMessageId = messageID
+            clientInfo = re.search("(?<=client=)([^[]+)\[([^]]+)",line)
+            if (clientInfo):
+                clientName = clientInfo.group(1)
+                clientIP = clientInfo.group(2)
+                print (f"Found client named {clientName} with IP {clientIP}")
+            mailServer = re.search("^[^ ]+\s([^ ]+)",line)
+            if (mailServer):
+                print (f"Found mailServer: {mailServer.group(1)}")
+        else:
+            pass
+            
 
 
 
